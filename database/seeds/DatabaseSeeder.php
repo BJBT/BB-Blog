f139a8916499de380cbccc99d3bb537bccec0e1f
Blayne Bennett <blaynebennett99@gmail.com>
2021-02-10 10:30:54 -0700

    added seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Comment;
+use App\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(Post::class, 20)->create();
+        factory(Comment::class, 20)->create();
+
     }
 }
